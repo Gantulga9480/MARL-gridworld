e7848bc8e92a8b092fcf9a7ea325dc3e7d184fee
Gantulga <limited.tulgaa@gmail.com>
2023-01-22 22:44:00 +0800

    stash

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from grid import GridEnv
 from RL import QLAgent
 
 environment = GridEnv(env_file='boards/board3.csv')
-agent = QLAgent()
+agent = QLAgent(environment.action_space, 0.1, 0.99, 0.999)
 agent.create_model((*environment.state_space, environment.action_space))
 
 environment.table = agent.model
@@ -10,7 +10,7 @@ environment.table = agent.model
 while environment.running:
     s = environment.reset()
     while not environment.loop_once():
-        a = agent.policy(s, use_e=False)
+        a = agent.policy(s, greedy=True)
         ns, r, d = environment.step(a)
         agent.learn(s, a, r, ns, d)
         s = ns
