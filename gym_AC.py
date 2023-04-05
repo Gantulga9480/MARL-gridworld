80dfc7e03491e419fc5224ccbf24eb86c2892142
Gantulga G <limited.tulgaa@gmail.com>
2023-04-05 20:38:54 +0800

    stash AC parallel

--- gym_AC.py
+++ gym_AC.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import numpy as np
 from RL.actor_critic import ActorCriticAgent
 import gym
 import matplotlib.pyplot as plt
@@ -38,20 +39,31 @@ class Critic(nn.Module):
 
 ENV_NAME = "CartPole-v1"
 TRAIN_ID = "ac_rewards_norm_loss_mean_itr5"
-env = gym.make(ENV_NAME, render_mode=None)
-agent = ActorCriticAgent(env.observation_space.shape[0], env.action_space.n, device="cuda:0")
-agent.create_model(Actor, Critic, actor_lr=0.001, critic_lr=0.001, gamma=0.99, entropy_coef=0.1, gae_lambda=1, reward_norm_factor=1)
+ENV_COUNT = 5
+envs = [gym.make(ENV_NAME, render_mode=None) for _ in range(ENV_COUNT)]
+agent = ActorCriticAgent(envs[0].observation_space.shape[0], envs[0].action_space.n, device="cuda:0")
+agent.create_model(Actor, Critic, actor_lr=0.0003, critic_lr=0.0001, gamma=0.99, entropy_coef=0.1, gae_lambda=1, env_count=ENV_COUNT, step_count=500, reward_norm_factor=1)
 
 try:
     while agent.episode_counter < 1000:
-        done = False
-        s, i = env.reset()
-        while not done:
-            a = agent.policy(s)[0]
-            ns, r, d, t, i = env.step(a)
-            done = d or t
-            agent.learn(s, a, ns, r, done)
-            s = ns
+        states = np.zeros((ENV_COUNT, envs[0].observation_space.shape[0]))
+        next_states = np.zeros((ENV_COUNT, envs[0].observation_space.shape[0]))
+        rewards = np.zeros(ENV_COUNT)
+        dones = np.zeros(ENV_COUNT)
+        for i, env in enumerate(envs):
+            s, _ = env.reset()
+            states[i] = s
+        while not any(dones):
+            actions = agent.policy(states)
+            for i, env in enumerate(envs):
+                rewards[i] = 0
+                if not dones[i]:
+                    ns, r, d, t, _ = env.step(actions[i])
+                    next_states[i] = ns
+                    rewards[i] = r
+                    dones[i] = d or t
+            agent.learn(states, actions, next_states, rewards, dones)
+            states = next_states
 except KeyboardInterrupt:
     pass
 env.close()
